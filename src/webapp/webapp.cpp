1189ad71737e1fd22f8964b380566788e4a8899a
Hansel <hansel@163.com>
2025-10-06 18:06:38 +0800

    webapp can run

--- src/webapp/webapp.cpp
+++ src/webapp/webapp.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
-#include "server.h"
+
 #include "common_const.h"
+#include "server.h"
+#include "webview.h"
+
+void start_web_view() {
+    // Create web view
+#ifdef _DEBUG
+    webview::webview w(true, nullptr);
+#else
+    webview::webview w(false, nullptr);
+#endif
+
+    // Set window title and size
+    w.set_title("Calculator");
+    w.set_size(800, 600, WEBVIEW_HINT_NONE);
+
+    // Navigate to the web server
+    std::string url = "http://" + CommonConst::WEB_ADDRESS + ":" + std::to_string(CommonConst::WEB_PORT);
+    w.navigate(url);
+
+    std::cout << "Client connecting to: " << url << std::endl;
+
+    // Run the web view event loop
+    w.run();
+}
 
 // Web server console application
 int main() {
@@ -15,8 +39,13 @@ int main() {
                   << ":" << server.getPort() << std::endl;
         std::cout << "Press Enter to stop server..." << std::endl;
 
-        // Wait for user input to stop
-        std::cin.get();
+        // Wait for the server to be ready
+        while (!server.isRunning()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        // Start the web view
+        start_web_view();
 
         // Stop the server
         server.stop();
